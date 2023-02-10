ba886d7138cd2a8763ede6f88e40773ecd71a2d2
GrzeCHu-a11y <ggonczowski@zsp5kozuchow.pl>
2023-02-10 17:10:40 +0100

    footer

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./footer.module.scss";
+import SectionHeader from "../../components/SectionHeader";
 
 const Footer = () => {
-  return <footer id="contact"></footer>;
+  return (
+    <footer id="contact" className={styles.footer}>
+      <SectionHeader title="Contact" />
+      <p>Email:</p>
+      <p>grzegorzgonczowski@gmai.com</p>
+      <p>Grzegorz Gończowski © 2023</p>
+    </footer>
+  );
 };
 
 export default Footer;
